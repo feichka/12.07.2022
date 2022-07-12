8d3091bab1bec80439e5cc907d5ad7270515b87e
«Natali» <feichka@yandex.ru>
2022-07-12 22:28:38 +0300

    Add task10

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 99 || n > 999)
+    Console.WriteLine("Число не трехзначное");
+else
+{
+int x;
+x = (n / 10) % 10;
+Console.WriteLine(x);
+}
