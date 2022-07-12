9cfb320731dd9a046418ca4d2d20956ce4abc162
«Natali» <feichka@yandex.ru>
2022-07-12 23:03:28 +0300

    Add task15

---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите цифру, соответствующую дню недели");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 || a == 7)
+    Console.WriteLine("Это выходной день");
+else
+{
+    if (a > 7)
+    Console.WriteLine("Так не бывает");
+    if (a < 6)
+    Console.WriteLine("это будний день");
+}
+    
