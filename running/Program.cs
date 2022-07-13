b17926b32106973b9db589c6436a2f581be37381
«Natali» <feichka@yandex.ru>
2022-07-13 09:04:23 +0300

    Add running

---
+++ running/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+double dis = x;
+int day = 1; // количество дней пробега
+while (dis < y)
+{
+    dis = dis + ((dis /100) * 15);
+    day++;
+}
+
+if (dis == y || dis > y)
+Console.WriteLine(day);
+
