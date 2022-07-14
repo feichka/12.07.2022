d85e0252b3ffc73ba2fc9614f5806ae5077a966b
«Natali» <feichka@yandex.ru>
2022-07-14 22:09:05 +0300

    Add bank

---
+++ bank/Program.cs
@@ -0,0 +1,14 @@
+﻿int x = Convert.ToInt32(Console.ReadLine()); // вклад в банке
+int p = Convert.ToInt32(Console.ReadLine()); // на столько процентов увеличивается вклад каждый год
+int y = Convert.ToInt32(Console.ReadLine()); //через сколько лет вклад составит У рублей
+double m = x;
+int t = 0; // через столько лет 
+while (m < y)
+{
+    m = m + ((m/100)*p);
+    m =  ((m * 100)- m %100) / 100;
+    t ++; 
+}
+if (m == y || m > y)
+Console.WriteLine(t);
+
