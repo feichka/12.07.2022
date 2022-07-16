cdd4ee9c153b0fbbd842b2bfb7fba15e56fbb053
«Natali» <feichka@yandex.ru>
2022-07-16 10:05:50 +0300

    Correction task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,6 +1,6 @@
-﻿int n = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите число, а я покажу его третью цифру или скажу, что ее нет!");
+int n = Convert.ToInt32(Console.ReadLine());
 if (n < 100)
-Console.WriteLine("Третьей цифры нет");
-int x;
-x = (n/100) % 10;
-Console.WriteLine(x);
+    Console.WriteLine("Третьей цифры нет :(");
+else
+    Console.WriteLine(Convert.ToString(n)[2]);
